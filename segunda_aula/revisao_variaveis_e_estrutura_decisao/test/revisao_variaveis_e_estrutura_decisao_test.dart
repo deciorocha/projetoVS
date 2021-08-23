6ae214d5bcf9dd249471e90a592525bc814793cf
deciorocha <deciolrocha@hotmail.com>
2021-08-23 11:18:25 -0300

    revisão - variáveis e estrutura de decisão

---
+++ segunda_aula/revisao_variaveis_e_estrutura_decisao/test/revisao_variaveis_e_estrutura_decisao_test.dart
@@ -0,0 +1,8 @@
+import 'package:revisao_variaveis_e_estrutura_decisao/revisao_variaveis_e_estrutura_decisao.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
