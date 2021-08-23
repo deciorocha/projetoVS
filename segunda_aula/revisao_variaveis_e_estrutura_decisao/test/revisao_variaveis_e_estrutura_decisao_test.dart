800c8879817979aaacbfc01cb5943a3a926e4709
deciorocha <deciolrocha@hotmail.com>
2021-08-23 11:56:13 -0300

    Apagar Arquivos Desnecessários

--- segunda_aula/revisao_variaveis_e_estrutura_decisao/test/revisao_variaveis_e_estrutura_decisao_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:revisao_variaveis_e_estrutura_decisao/revisao_variaveis_e_estrutura_decisao.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
