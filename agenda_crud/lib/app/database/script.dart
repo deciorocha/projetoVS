0d8152f83f5277af3bd0c13c4385523833f7e8c5
deciorocha <deciolrocha@hotmail.com>
2021-09-16 17:30:04 -0300

    Ainda Com erros

---
+++ agenda_crud/lib/app/database/script.dart
@@ -0,0 +1,36 @@
+final createTable = '''
+CREATE TABLE cotact(
+id INT PRIMARY KEY, 
+nome VARCHAR(200) NOT NULL,
+telefone CHAR(16) NOT NULL,
+email VARCHAR(150) NOT NULL,
+url_avatar VACHAR(300) NOT NULL
+)
+''';
+
+final insertRegistros = '''
+INSERT INTO contact (nome, telefone, email, url_avatar)
+VALUES (
+  'Hortência',
+  '(11) 9 9562-3356',
+  'hortencia@email.com',
+  'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+);
+
+INSERT INTO contact (nome, telefone, email, url_avatar)
+VALUES (
+  'Maitê',
+  '(11) 9 9632-8578',
+  'maite@email.com',
+  'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+);
+
+INSERT INTO contact (nome, telefone, email, url_avatar)
+VALUES (
+  'Pietro',
+  '(11) 9 9874-5656',
+  'pietro@email.com',
+  'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
+);
+
+''';
---
+++ meu_teste/android/app/src/main/kotlin/br/com/deciorocha/meu_teste/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.deciorocha.meu_teste
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
